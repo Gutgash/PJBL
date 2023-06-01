847ff537760871612e15d05c3c5585b5ad65c13a
Arthur <132582569+Gutgash@users.noreply.github.com>
2023-06-01 19:45:45 -0300

    Add files via upload

--- Login.java
+++ Login.java
@@ -1,16 +1,90 @@
-public class Login {
-    private String login;
-    private String senha;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.Exception;
 
-    public Login(String login, String senha){
-        this.login = login;
-        this.senha = senha;
-    }
-    public String GetUsuario(){
-        return login;
-    }
-    public String GetSenha(){
-        return senha;
+class CreateLoginForm extends JFrame implements ActionListener
+{
+
+    JButton b1;
+    JPanel newPanel;
+    JLabel userLabel, passLabel;
+    final JTextField  textField1, textField2;
+
+    //calling constructor
+    CreateLoginForm()
+    {
+
+        userLabel = new JLabel();
+        userLabel.setText("Username");
+
+        textField1 = new JTextField(15);
+
+        passLabel = new JLabel();
+        passLabel.setText("Password");
+
+        textField2 = new JPasswordField(15);
+
+        b1 = new JButton("SUBMIT");
+
+        newPanel = new JPanel(new GridLayout(3, 1));
+        newPanel.add(userLabel);
+        newPanel.add(textField1);
+        newPanel.add(passLabel);
+        newPanel.add(textField2);
+        newPanel.add(b1);
+
+
+        add(newPanel, BorderLayout.CENTER);
+
+        b1.addActionListener(this);
+        setTitle("LOGIN FORM");
     }
 
+    public void actionPerformed(ActionEvent ae)
+    {
+        String userValue = textField1.getText();
+        String passValue = textField2.getText();
+
+        if (userValue.equals("test#0000") && passValue.equals("test")) {
+
+            NewPage page = new NewPage();
+
+            page.setVisible(true);
+
+            JLabel wel_label = new JLabel("Welcome: "+userValue);
+            page.getContentPane().add(wel_label);
+        }
+        else{
+            System.out.println("Please enter valid username and password");
+        }
+    }
+}
+
+class LoginFormDemo
+{
+    public static void main(String arg[])
+    {
+        try
+        {
+            CreateLoginForm form = new CreateLoginForm();
+            form.setSize(300,100);
+            form.setVisible(true);
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+}
+
+class NewPage extends JFrame
+{
+    NewPage()
+    {
+        setDefaultCloseOperation(javax.swing.
+                WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Welcome");
+        setSize(400, 200);
+    }
 }
