da6baeda8b549083a0bc317ad6b2b075ff3360fe
Arthur <132582569+Gutgash@users.noreply.github.com>
2023-06-12 16:37:17 -0300

    Add files via upload

--- MarketplaceInterface.java
+++ MarketplaceInterface.java
@@ -1,28 +1,84 @@
+package components;
 import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
 import javax.swing.*;
+import components.txtReader;
 
-public class MarketplaceInterface extends JFrame
-{
-    JComboBox comboBox;
+public class MarketplaceInterface extends JPanel implements ActionListener {
+    JLabel picture;
 
-    public MarketplaceInterface()
-    {
-        Object[] items =
-                {
-                        new ImageIcon("C:\\Users\\NTC\\IdeaProjects\\untitled\\Images\\Lindomar.jpg"),
-                        new ImageIcon("C:\\Users\\NTC\\IdeaProjects\\untitled\\Images\\Shia.jpg"),
-                        new ImageIcon("C:\\Users\\NTC\\IdeaProjects\\untitled\\Images\\Cage.jpg")
-                };
-        comboBox = new JComboBox( items );
-        getContentPane().add( comboBox, BorderLayout.NORTH );
+    public MarketplaceInterface() {
+        super(new BorderLayout());
+
+        String[] skinsStrings = { "Vandal Prime", "Vandal Sakura", "Phantom Nebula", "Phantom Glitchpop"};
+
+        JComboBox petList = new JComboBox(skinsStrings);
+        petList.setSelectedIndex(3);
+        petList.addActionListener(this);
+
+        picture = new JLabel();
+        picture.setFont(picture.getFont().deriveFont(Font.ITALIC));
+        picture.setHorizontalAlignment(JLabel.CENTER);
+        updateLabel(skinsStrings[petList.getSelectedIndex()]);
+        picture.setBorder(BorderFactory.createEmptyBorder(10,0,0,0));
+        picture.setPreferredSize(new Dimension(1000, 700+10));
+
+        add(petList, BorderLayout.PAGE_START);
+        add(picture, BorderLayout.PAGE_END);
+        setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
     }
 
-    public static void main(String[] args)
-    {
-        JFrame frame = new MarketplaceInterface();
-        frame.setDefaultCloseOperation( EXIT_ON_CLOSE );
+
+    public void actionPerformed(ActionEvent e) {
+        JComboBox cb = (JComboBox)e.getSource();
+        String petName = (String)cb.getSelectedItem();
+        updateLabel(petName);
+    }
+
+    protected void updateLabel(String name) {
+        ImageIcon icon = createImageIcon("/images/" + name + ".png");
+        /* cria a variável de texto */
+
+        picture.setIcon(icon);
+        picture.setToolTipText("A drawing of a " + name.toLowerCase());
+        /* posição do texto */
+
+        if (icon != null) {
+            picture.setText(null);
+            /* printa o texto */
+        } else {
+            picture.setText("Image not found");
+        }
+    }
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = MarketplaceInterface.class.getResource(path);
+
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Rifles");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JComponent newContentPane = new MarketplaceInterface();
+        newContentPane.setOpaque(true);
+        frame.setContentPane(newContentPane);
+
         frame.pack();
-        frame.setLocationRelativeTo( null );
-        frame.setVisible( true );
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
