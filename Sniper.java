3546bb36153505b8f65bb55ba8f69cf899e5e844
Arthur <132582569+Gutgash@users.noreply.github.com>
2023-06-22 12:35:05 -0300

    Delete Sniper.java

--- Sniper.java
+++
@@ -1,5 +0,0 @@
-public class Sniper extends Skin {
-    public Sniper(String nome, String corPrimaria, String corSecundaria, float preco) {
-        super(nome, corPrimaria, corSecundaria, preco);
-    }
-}
